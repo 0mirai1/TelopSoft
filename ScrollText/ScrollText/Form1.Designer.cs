c27b0ffa3774eb36cdda37bc4a53ab8267a2c644
みらい <105960405+0mirai1@users.noreply.github.com>
2022-05-21 13:58:34 +0900

    Add files via upload

---
+++ ScrollText/ScrollText/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace ScrollText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.Mainlabel = new System.Windows.Forms.Label();
+            this.TelopTimer = new System.Windows.Forms.Timer(this.components);
+            this.MainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.Mainlabel);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1384, 51);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // Mainlabel
+            // 
+            this.Mainlabel.AutoSize = true;
+            this.Mainlabel.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Mainlabel.ForeColor = System.Drawing.Color.Black;
+            this.Mainlabel.Location = new System.Drawing.Point(562, 7);
+            this.Mainlabel.Name = "Mainlabel";
+            this.Mainlabel.Size = new System.Drawing.Size(196, 37);
+            this.Mainlabel.TabIndex = 0;
+            this.Mainlabel.Text = "TelopLabel";
+            // 
+            // TelopTimer
+            // 
+            this.TelopTimer.Enabled = true;
+            this.TelopTimer.Interval = 1;
+            this.TelopTimer.Tick += new System.EventHandler(this.TelopTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 51);
+            this.Controls.Add(this.MainPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Label Mainlabel;
+        public System.Windows.Forms.Timer TelopTimer;
+    }
+}
+
---
+++ ScrollText/ScrollText/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScrollText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void TelopTimer_Tick(object sender, EventArgs e)
+        {
+            Mainlabel.Left += -2;//テロップ速度
+            if (Mainlabel.Right < 0)  Mainlabel.Left = this.ClientSize.Width - Mainlabel.Right;//一番左に行ったら右へlabelを戻す
+        }
+    }
+}
